3f80e24b7b993b6d40415a3f9e6532aa465eb70c
qhuy071003 <qhuy071003@gmail.com>
2024-06-07 02:47:26 +0700

    update-VNPAY

---
+++ src/main/java/com/hutech/VoTranQuocHuy430/controller/PaymentController.java
@@ -0,0 +1,83 @@
+package com.hutech.VoTranQuocHuy430.controller;
+
+import com.hutech.VoTranQuocHuy430.config.Config;
+import com.hutech.VoTranQuocHuy430.DTO.PaymentResDTO;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+@RestController
+@RequestMapping("/api/payment")
+@CrossOrigin(origins = "*")
+public class PaymentController {
+
+    @GetMapping("/create-payment")
+    public ResponseEntity<PaymentResDTO> createPayment(HttpServletRequest req) throws UnsupportedEncodingException {
+        String vnp_TxnRef = Config.getRandomNumber(8);
+        String vnp_IpAddr = Config.getIpAddress(req);
+        String vnp_TmnCode = Config.vnp_TmnCode;
+        String vnp_HashSecret = Config.secretKey;
+
+        Map<String, String> vnp_Params = new HashMap<>();
+        vnp_Params.put("vnp_Version", Config.vnp_Version);
+        vnp_Params.put("vnp_Command", Config.vnp_Command);
+        vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
+        vnp_Params.put("vnp_Amount", "1000000");
+        vnp_Params.put("vnp_CurrCode", "VND");
+        vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
+        vnp_Params.put("vnp_OrderInfo", "Thanh toan don hang test");
+        vnp_Params.put("vnp_OrderType", "other");
+        vnp_Params.put("vnp_Locale", "vn");
+        vnp_Params.put("vnp_ReturnUrl", Config.vnp_ReturnUrl);
+        vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
+
+        // Add vnp_CreateDate and vnp_ExpireDate
+        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        String vnp_CreateDate = formatter.format(cld.getTime());
+        vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
+
+        cld.add(Calendar.MINUTE, 15);
+        String vnp_ExpireDate = formatter.format(cld.getTime());
+        vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
+
+        List<String> fieldNames = new ArrayList<>(vnp_Params.keySet());
+        Collections.sort(fieldNames);
+        StringBuilder hashData = new StringBuilder();
+        StringBuilder query = new StringBuilder();
+        for (String fieldName : fieldNames) {
+            String fieldValue = vnp_Params.get(fieldName);
+            if ((fieldValue != null) && (fieldValue.length() > 0)) {
+                hashData.append(fieldName);
+                hashData.append('=');
+                hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
+                query.append('=');
+                query.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                if (fieldNames.indexOf(fieldName) < fieldNames.size() - 1) {
+                    query.append('&');
+                    hashData.append('&');
+                }
+            }
+        }
+        String queryUrl = query.toString();
+        String vnp_SecureHash = Config.hmacSHA512(Config.secretKey, hashData.toString());
+        queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
+        String paymentUrl = Config.vnp_PayUrl + "?" + queryUrl;
+
+        PaymentResDTO paymentResDTO = new PaymentResDTO();
+        paymentResDTO.setStatus("ok");
+        paymentResDTO.setMessage("successfully");
+        paymentResDTO.setURL(paymentUrl);
+
+        return ResponseEntity.status(HttpStatus.OK).body(paymentResDTO);
+    }
+}
